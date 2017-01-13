4c03939b8a59044e83120888d069e0b4a2a0a580
ajnix <ajnix>
2017-01-13 14:04:47 +0000

    Replaced the clock with digital timer

--- js/time.js
+++ js/time.js
@@ -1,4 +1,24 @@
 (function() {
+  var contador = 0;
+
+  setInterval(setDates, 1000);
+
+  function setDates() {
+    var date = new Date;
+    var hour = date.getHours(),
+        minutes = (date.getMinutes() < 10) ? '0' + String(date.getMinutes()):
+                  date.getMinutes();
+        hourTitle = document.getElementById('hour');
+
+    if (!contador) {
+      hourTitle.textContent = hour + ':' + minutes;
+      contador = 1;
+    }
+    else {
+      hourTitle.innerHTML = hour + '<span class="hidden">:</span>' +
+                            minutes;
+      contador = 0;
+    }
 
     var day = date.getDate(),
         month = date.getMonth(),
